b807a9580fae11dbdb96b589809cda79b108de62
nka4349-alt <nka4349@gmail.com>
2026-09-10 18:29:03 +0900

    Separate Render build from database migrations

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-bundle install
-bundle exec rails db:migrate
+BUNDLE_FROZEN=true bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
